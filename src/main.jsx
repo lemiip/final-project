0f8f54499f368828ff099618a21756af38cf63be
lemiip <lemipiri27@gmail.com>
2026-06-29 05:52:43 +0400

    new2

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import {CartProvider} from "./context/CartContext";
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.jsx'
+
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <CartProvider>
+  <>
     <App />
-  </CartProvider>,
+  </>,
 )
